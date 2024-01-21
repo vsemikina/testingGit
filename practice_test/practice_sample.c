81c9b43098784d11148a4361ee28e8eba86dd407
vsemikina <varya.semikina@gmail.com>
2024-01-21 13:35:51 +0100

    First change made
    Added sentenses, bla bla

---
+++ practice_test/practice_sample.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+struct Circle {
+	double radius;
+	int x, y;
+	char* color;
+};
+
+struct Square {
+	double side;
+	int x, y;
+	char* color;
+
+};
+
+typedef struct Square Square;
+typedef struct Circle Circle;
+
+int circleFitsInSquare(struct Circle* circle, struct Square* square) {
+    return (circle->radius * 2 <= square->side);
+}
+
+int squareFitsInCircle(struct Square* square, struct Circle* circle) {
+    double diagonal = sqrt(2) * square->side;
+    return (diagonal <= 2 * circle->radius);
+}
+
+void printResult(struct Circle* circle, struct Square* square) {
+    if (circleFitsInSquare(circle, square)) {
+        printf("The circle fits into the square.\n");
+    }
+    else {
+        printf("The circle does not fit into the square.\n");
+    }
+
+    if (squareFitsInCircle(square, circle)) {
+        printf("The square fits into the circle.\n");
+    }
+    else {
+        printf("The square does not fit into the circle.\n");
+        //printf("I am adding this sentenses to check github");
+    }
+}
+
+int main() {
+
+    struct Circle myCircle = { 5.0, 0, 0, "Red" };
+    struct Square mySquare = { 10.0, 0, 0, "Blue" };
+
+    printResult(&myCircle, &mySquare);
+
+    printf("here is another sentense to test github");
+
+
+    return 0;
+}
+
+
